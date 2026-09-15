d35f81c1d0caa377de0fe1a907109356d52db28c
abhinav <abhinavdev04@gmail.com>
2026-09-15 09:44:04 +0545

    Add Network Programming Lab 1 q2 and q3 questions

--- lab1/Q1FindHostname.java
+++ lab1/Q1FindHostname.java
@@ -1,7 +1,7 @@
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-public class FindHostname {
+public class Q1FindHostname {
     public static void main(String[] args) {
         String ipAddress = "8.8.8.8";
 
---
+++ lab1/Q3IPaddressTest.java
@@ -0,0 +1,25 @@
+import java.net.UnknownHostException;
+import java.net.InetAddress;
+
+public class Q3IPaddressTest {
+    public static void main(String[] args) {
+
+        String ipAddress = "FF00::1";
+
+        try {
+            InetAddress address = InetAddress.getByName(ipAddress);
+
+            if (address instanceof java.net.Inet4Address) {
+                System.out.println(ipAddress + " is an IPv4 address.");
+            } else if (address instanceof java.net.Inet6Address) {
+                System.out.println(ipAddress + " is an IPv6 address.");
+            } else {
+                System.out.println(ipAddress + " is not a valid IP address.");
+            }
+
+        } catch (UnknownHostException e) {
+            System.err.println("Unable to resolve the IP address: " + ipAddress);
+            e.printStackTrace();
+        }
+    }
+}
